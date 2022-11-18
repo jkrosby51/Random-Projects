1bf3f50c322b2f96329eedf26333b0d05b0fb3da
jkrosby51 <56936867+jkrosby51@users.noreply.github.com>
2022-11-18 11:51:37 -0500

    Update connectFour.py

--- random/connectFour.py
+++ random/connectFour.py
@@ -8,12 +8,14 @@ def setBoard():
   return board
 
 def printBoard(board):
+  print()
   print(board)
   print("   0   1   2   3   4   5   6")
 
 def findLowest(board, x):
-  for y in range(5, 0):
-    if board[x][y] == '_':
+  for y in range(5, 0, -1):
+    #print(f"board[x][y] == {board[x][y]}")
+    if board[y][x] == "_":
       return y
   return -1
 
@@ -25,11 +27,12 @@ while True:
     printBoard(board)
     badlyNamedBool = True # stopper 
     while badlyNamedBool == True:
+      print()
       userInput = input("Player 1 (X), where would you like to play? ")
       try:
         userInput = int(userInput)
   
-        if userInput <= 0 and userInput <= 6:
+        if userInput >= 0 and userInput <= 6:
           if findLowest(board, userInput) != -1:
             badlyNamedBool = False
           else:
@@ -38,6 +41,27 @@ while True:
           print("Unknown column.")
       except:
         print("Must be an Integer (0-6).")
+    board[findLowest(board, userInput)][userInput] = 'X'
+
+    printBoard(board)
+    
+    badlyNamedBool = True
+    while badlyNamedBool == True:
+      print()
+      userInput = input("Player 2 (O), where would you like to play? ")
+      try:
+        userInput = int(userInput)
+  
+        if userInput >= 0 and userInput <= 6:
+          if findLowest(board, userInput) != -1:
+            badlyNamedBool = False
+          else:
+            print("That column is full.")
+        else:
+          print("Unknown column.")
+      except:
+        print("Must be an Integer (0-6).")
+    board[findLowest(board, userInput)][userInput] = 'O'
             
     
     
